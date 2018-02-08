4cabce028af19144c528e22fb25210dddbfcb0bd
thatrobotguy <andrewtheschueler@gmail.com>
2018-02-07 19:14:07 -0500

    Fixed our labs

--- Lab_3/Lab_3_Part_2.m
+++ Lab_3/Lab_3_Part_2.m
@@ -1,25 +1,22 @@
-close all; clear all; clc
-syms s;
+close all; clear all; clc; syms s;
 
 Induct = 5;             % Henries
 Cap = 300;              % Farads
-Resist = 100;        % Ohms 100k ohms    
+Resist = 100;           % Ohms 100k ohms    
 Volts = 5;              % Input Voltage
 
-A = ([0 1; -1/(Cap*Induct) -Resist/Induct]);
-B = ([0; 1/Induct]);  C = ([1 0; 0 0]);    D = ([0; 0]);
-
-phi = inv((s.*eye(2))-A);                 H = C*phi*B+D;
+A =([0 1; -1/(Cap*Induct) -Resist/Induct]);
+B =([0; 1/Induct]); C =([1 0; 0 0]); D =([0; 0]);
+phi = inv((s.*eye(2))-A); H = C*phi*B+D;
 pretty(simplify(H));
 
-mytf1 = tf([0 0 300], [1500 30000 1]);
-mytf2 = tf([0 0 0 300], [1500 30000 1 0]);
-figure(1);
-step(mytf1);
-figure(2);
-impulse(mytf1);
-figure(3);
-step(mytf2);
-title("Ramp Respone");
+% mytf1 = tf([0 0 300], [1500 30000 1]); mytf2 = tf([0 0 0 300], [1500 30000 1 0]);
+mytf1 = tf([  0 0 1], [   1 Resist/Induct 1/(Cap*Induct) ]);
+mytf2 = tf([0 0 0 1], [ 1 Resist/Induct 1/(Cap*Induct) 0 ]);
+
+figure(1); step(mytf1);
+figure(2); impulse(mytf1);
+figure(3); step(mytf2);    title("Ramp Response");
+
 disp(stepinfo(mytf1));
-% disp(stepinfo(impulse(mytf)));
+% disp(stepinfo(mytf2));
