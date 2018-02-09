7d3450a28bcaec3689ced86cefdb159754db8768
thatrobotguy <andrewtheschueler@gmail.com>
2018-02-08 20:08:09 -0500

    Added the zeta thing

--- Lab_3/calcZeta.m
+++ Lab_3/calcZeta.m
@@ -1,5 +1,16 @@
-function Output = calcZetaOmegaNOmegaD(numerator, denominator)
+% The inputs are the row vectors input into the transfer function.
 
+function Output = calcZetaOmegaNOmegaD(numerator, denominatorSecLast, ...
+    denominatorLast)
 
+% k omegan ^2 is in the numerator
+% The denom. middle is 2*zeta*omegan = middle cooeficient
+% The denom. end is omegan^2
+
+omegan = denominatorLast^0.5;
+k = numerator/denominatorLast;
+zeta = denominatorSecLast/(2*omegan);
+
+Output = [k omegan zeta];
 
 end
